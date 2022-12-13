76633dcd146a033ef6295b439d6fa951c1f4fb84
nguyenhaivanz <haivan090824@gmail.com>
2022-12-13 16:27:28 +0700

    update moi

--- src/main/java/com/example/md4/controller/AuthController.java
+++ src/main/java/com/example/md4/controller/AuthController.java
@@ -42,10 +42,10 @@ public class AuthController {
                 new UsernamePasswordAuthenticationToken(account.getGmail(), account.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         String jwt = jwtService.generateTokenLogin(authentication);
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         Account currentUser = accountService.findByGmail(account.getGmail()).get();
         return ResponseEntity.ok(new JwtResponse(currentUser.getId(), jwt, userDetails.getUsername(), userDetails.getAuthorities()));
     }
+
 }
